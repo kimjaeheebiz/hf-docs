1af059cd99d319691c86bcabaaeb53dec105955f
KIMJAEHEE\jaehe <kimjaeheebiz@gmail.com>
2025-09-08 00:41:19 +0900

    패키지 정리, 샘플 페이지 생성

--- pages/_meta.js
+++ pages/_meta.js
@@ -6,12 +6,12 @@ export default {
     title: 'About',
     type: 'page'
   },
-  contact: {
-    title: 'Contact ↗',
-    type: 'page',
-    href: 'https://twitter.com/shuding_',
-    newWindow: true
-  }
+  // contact: {
+  //   title: 'Contact ↗',
+  //   type: 'page',
+  //   href: 'https://twitter.com/shuding_',
+  //   newWindow: true
+  // }
 }
 
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -4,35 +4,34 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: (
     <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-      <img src="/images/logo.svg" alt="Hecto Financial" width={24} height={24} />
+      <img src="/images/logo.svg" alt="Hecto Financial" style={{ width: 'auto', height: 20 }} />
       <span style={{ fontWeight: 600 }}>개발 지원</span>
     </div>
   ),
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   navbar: {
     extraContent: (
       <div style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
-        <nav style={{ display: 'flex', gap: 16 }}>
+        {/* <nav style={{ display: 'flex', gap: 16 }}>
           <a href="/guides">가이드</a>
           <a href="/sandbox">샌드박스</a>
           <a href="/blog">블로그</a>
-        </nav>
-        <span aria-hidden>🔍</span>
-        <a href="/login">로그인</a>
+        </nav> */}
+        <a href="https://develop.sbsvc.online/login.do" target="_blank" rel="noopener noreferrer">로그인</a>
       </div>
     )
   },
   search: {
-    placeholder: '🔍 검색'
+    placeholder: '검색'
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    content: 'Nextra Docs Template',
+    content: 'Copyright 2025 Hecto Financial',
   },
 }
 

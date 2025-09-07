6a71506386790aabe400855fa3552fc6f6e41bf3
KIMJAEHEE\jaehe <kimjaeheebiz@gmail.com>
2025-09-08 00:56:44 +0900

    서체 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import 'nextra-theme-docs/style.css'
+// Pretendard 공식 패키지 CSS를 먼저 로드하여 폰트 파일을 확실히 포함
+import 'pretendard/dist/web/static/pretendard.css'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
